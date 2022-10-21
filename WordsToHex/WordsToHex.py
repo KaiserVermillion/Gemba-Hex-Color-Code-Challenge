3ab0bc0c1fbb627e1b222f6f39ba66e8f5734ac7
unknown <kogilanarunthavalingham@gmail.com>
2022-10-21 18:49:42 +0100

    Moved the words git folder into the assets folder and updated the README.md

--- WordsToHex/WordsToHex.py
+++ WordsToHex/WordsToHex.py
@@ -85,57 +85,6 @@ class HexGenerator:
             # print(hexWords)
         return
 
-    # def divideAndConq(self,word):
-    #     """
-    #     divideAndConq divides a word into 2 and converts the values which can be converted from and identifies the 
-    #     differnt combinations of the values that are avalible. The assumption made here is that there is no word that 
-    #     wil satisfy.
-
-    #     :param word: A string
-    #     :return: A list of words which contains all the possible combinations of the string reconstructed from the 4
-    #              parts listContA, listContB, strA and strB
-    #     """
-    #     listContA = ""
-    #     listContB = ""
-    #     strA = word[:len(word)//2]
-    #     strB = word[len(word)//2:]
-
-    #     for i in range(len(strA)):
-    #         tempLetter = strA[i]
-    #         if tempLetter in letterMap.keys():
-    #             listContA = strA.replace(tempLetter, letterMap[tempLetter])
-
-    #     for i in range(len(strB)):
-    #         tempLetter = strB[i]
-    #         if tempLetter in letterMap.keys():
-    #             listContB = strB.replace(tempLetter, letterMap[tempLetter])
-
-    #     return [strA+listContB,strA+strB,listContA+strB,listContA+listContB]
-
-
-    # # Generates a new string by replacing values which 
-    # # Solution 1
-    # # If its already a hex value in the string dont replace it
-    # def wordToHex(self, word):
-    #     """
-    #     wordToHex
-    #     """
-    #     counter = 0
-
-    #     for x in range(len(word)):
-    #         if word[x] in letterMap.keys() and word[x] in hexAlpha:
-    #             counter += 1
-
-    #     if counter == 2 :
-    #         return self.divideAndConq(word)
-    #     else:
-    #         for i in range(len(word)):
-    #             # tempLetter = word[i]
-
-    #             if word[i] in letterMap.keys():
-    #                 word = word.replace(word[i], letterMap[word[i]])
-
-    #     return word
 
     def createBinaryRepresentation(self,word):
         """
@@ -248,7 +197,7 @@ class HexGenerator:
 
         :params hexList: is a list of string which should have been converted and processed into hex codes
         """
-        file = open("C:/Users/Kog Arun/Documents/Gemba Advantage Coding Test/assets/HexWords", "w")
+        file = open("./assets/HexWords", "w")
         for tmp in hexList:
             for i in tmp:
                 file.write("%s\n" % i)
